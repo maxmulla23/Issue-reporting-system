01db9444b818a22b7c04a5175fe147e676512e17
maxmulla23 <maxmulla23@gmailcom>
2023-12-11 16:53:12 +0300

    dbcontextadded

--- Models/IssueContext.cs
+++ Models/IssueContext.cs
@@ -4,9 +4,10 @@ namespace IssueReport.Models;
 
 public class IssueReportContext : DbContext
 {
+    
     public DbSet<User>? Users { get; set; }
     public DbSet<UserRole>? UserRoles { get; set; }
     public DbSet<Recommendations>? Recommendations { get; set; }
-    public DbSet<BugIssue>? bugIssues { get; set; }
+    public DbSet<BugIssue>? BugIssues { get; set; }
     public DbSet<Tasks>? Tasks { get; set; }
 }
